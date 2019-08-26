2b86f98775f586a601392de66ef01466694df493
freearhey <free.arhey@gmail.com>
2019-08-26 04:16:45 +0300

    Removed social buttons from footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -3,8 +3,6 @@
     .container
       .content.has-text-centered
         p Designed by #[a(:href="'https://github.com/' + app.github.username",target="_blank",rel="noopener noreferrer") {{ app.author }}]. Licensed under #[a(href="http://opensource.org/licenses/MIT") MIT].
-        p.social-buttons
-          span(v-for="link in app.links",v-html="link")
         p
           a.icon(:href="'https://github.com/' + app.github.repo",target="_blank",rel="noopener noreferrer",aria-label="GitHub")
             i.ion-social-github
