427e98d58018b0b03e2ea3bd22a0db2193366f5e
freearhey <free.arhey@gmail.com>
2019-10-13 21:29:37 +0300

    Replaced cross-env with dotenv

--- webpack.config.js
+++ webpack.config.js
@@ -1,110 +1,97 @@
+require('dotenv').config()
 const webpack = require('webpack')
 const path = require('path')
-const CopyWebpackPlugin = require('copy-webpack-plugin')
 const VueLoaderPlugin = require('vue-loader/lib/plugin')
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
 
-const ASSET_PATH = process.env.ASSET_PATH || '/build/'
-
-module.exports = {
-  entry: './src/main.js',
-  mode: 'development',
-  output: {
-    path: path.resolve(__dirname, 'build'),
-    publicPath: ASSET_PATH,
-    filename: 'bundle.js'
-  },
-  devtool: 'inline-source-map',
-  devServer: {
-    historyApiFallback: true
-  },
-  plugins: [
-    new VueLoaderPlugin()
-  ],
-  resolve: {
-    alias: {
-      '~': path.resolve(__dirname, 'src/'),
-      '~api': path.resolve(__dirname, 'src/api/'),
-      '~assets': path.resolve(__dirname, 'src/assets/'),
-      '~components': path.resolve(__dirname, 'src/components/'),
-      '~config': path.resolve(__dirname, 'src/weblearn.config.js'),
-      '~events': path.resolve(__dirname, 'src/events/'),
-      '~pages': path.resolve(__dirname, 'src/pages/'),
-      '~store': path.resolve(__dirname, 'src/store/'),
-      '~topics': path.resolve(__dirname, 'src/topics/'),
-      '~transformers': path.resolve(__dirname, 'src/transformers/'),
-      '~util': path.resolve(__dirname, 'src/util/'),
-      '~widgets': path.resolve(__dirname, 'src/widgets/')
-    }
-  },
-  module: {
-    rules: [
-      {
-        test: /\.vue$/,
-        loader: 'vue-loader',
-        options: {
-          loaders: {
-            sass: 'vue-style-loader!css-loader!sass-loader?indentedSyntax',
-            scss: 'vue-style-loader!css-loader!sass-loader'
+module.exports = env => {
+  let config = {
+    mode: 'development',
+    entry: './src/main.js',
+    output: {
+      path: path.resolve(__dirname, 'build'),
+      publicPath: '/build/',
+      filename: 'bundle.js'
+    },
+    devtool: 'inline-source-map',
+    devServer: {
+      historyApiFallback: true
+    },
+    plugins: [
+      new VueLoaderPlugin(),
+      new webpack.DefinePlugin({
+        'process.env.YOUTUBE_API_KEY': `"${process.env.YOUTUBE_API_KEY}"`
+      })
+    ],
+    resolve: {
+      alias: {
+        '~': path.resolve(__dirname, 'src/'),
+        '~api': path.resolve(__dirname, 'src/api/'),
+        '~assets': path.resolve(__dirname, 'src/assets/'),
+        '~components': path.resolve(__dirname, 'src/components/'),
+        '~config': path.resolve(__dirname, 'src/weblearn.config.js'),
+        '~events': path.resolve(__dirname, 'src/events/'),
+        '~pages': path.resolve(__dirname, 'src/pages/'),
+        '~store': path.resolve(__dirname, 'src/store/'),
+        '~topics': path.resolve(__dirname, 'src/topics/'),
+        '~transformers': path.resolve(__dirname, 'src/transformers/'),
+        '~util': path.resolve(__dirname, 'src/util/'),
+        '~widgets': path.resolve(__dirname, 'src/widgets/')
+      }
+    },
+    module: {
+      rules: [
+        {
+          test: /\.vue$/,
+          loader: 'vue-loader',
+          options: {
+            loaders: {
+              sass: 'vue-style-loader!css-loader!sass-loader?indentedSyntax',
+              scss: 'vue-style-loader!css-loader!sass-loader'
+            }
+          }
+        },
+        {
+          test: /\.js$/,
+          loader: 'babel-loader',
+          exclude: /node_modules/
+        },
+        {
+          test: /\.json$/,
+          loader: 'json-loader'
+        },
+        {
+          test: /\.(yml|yaml)$/,
+          loader: 'json-loader!yaml-loader'
+        },
+        {
+          test: /\.pug$/,
+          loader: 'pug-plain-loader'
+        },
+        {
+          test: /\.sass$/,
+          loader: 'style-loader!css-loader!sass-loader?indentedSyntax'
+        },
+        {
+          test: /\.scss$/,
+          loader: 'style-loader!css-loader!sass-loader'
+        },
+        {
+          test: /\.(png|jpg|gif|svg)$/,
+          loader: 'file-loader',
+          query: {
+            name: '[name].[ext]?[hash]'
           }
         }
-      },
-      {
-        test: /\.js$/,
-        loader: 'babel-loader',
-        exclude: /node_modules/
-      },
-      {
-        test: /\.json$/,
-        loader: 'json-loader'
-      },
-      {
-        test: /\.(yml|yaml)$/,
-        loader: 'json-loader!yaml-loader'
-      },
-      {
-        test: /\.pug$/,
-        loader: 'pug-plain-loader'
-      },
-      {
-        test: /\.sass$/,
-        loader: 'style-loader!css-loader!sass-loader?indentedSyntax'
-      },
-      {
-        test: /\.scss$/,
-        loader: 'style-loader!css-loader!sass-loader'
-      },
-      {
-        test: /\.(png|jpg|gif|svg)$/,
-        loader: 'file-loader',
-        query: {
-          name: '[name].[ext]?[hash]'
-        }
-      }
-    ]
+      ]
+    }
   }
-}
 
-if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'analyzer') {
-  module.exports.mode = 'production'
-  module.exports.plugins = (module.exports.plugins || []).concat([
-    new CopyWebpackPlugin([
-      { from: 'src/static' }
-    ]),
-    new webpack.DefinePlugin({
-      'process.env': {
-        NODE_ENV: '"production"'
-      }
-    }),
-    new webpack.LoaderOptionsPlugin({
-      minimize: true
-    }),
-    new webpack.optimize.OccurrenceOrderPlugin()
-  ])
-
-  if(process.env.NODE_ENV === 'analyzer') {
-    module.exports.plugins = (module.exports.plugins || []).concat([
+  if(env && env.analyze) {
+    config.plugins = (config.plugins || []).concat([
       new BundleAnalyzerPlugin()
     ])
   }
+
+  return config
 }
